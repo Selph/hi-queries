d2014527f7943f1a1d2d5afbf0662850a7dba9f4
Selph <72403805+Selph@users.noreply.github.com>
2022-11-22 14:27:33 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, escape
+
+import util.tokenizer
+ 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    question = str(escape(request.args.get("question", "")))
+    if question:
+        answer = answer_from(question)
+    else:
+        answer = ""
+    return (
+        render_template('page.html', answer=answer)
+    )
+
+def answer_from(question):
+    """Svarar spurningu um HÍ"""
+    try:
+        return answer_algorithm(str(question))
+    except ValueError:
+        return "Ég veit ekki svarið... reyndu aftur!"
+    
+def answer_algorithm(question):
+    tokens = util.tokenizer.question_processing2(question)
+    return tokens
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
+    
---
+++ util/tokenizer.py
@@ -0,0 +1,41 @@
+import re
+import os
+import spacy
+
+from islenska import Bin
+
+b = Bin()
+
+nlp = spacy.load("en_core_web_sm")
+
+stop_words_path = os.getcwd()+'/txts/stop_words_english.txt'
+stop_words = open(stop_words_path, 'r')
+
+def custom_tokenizer(x):
+    pattern = re.compile(r'(?x) \w\.[áí]\s\w\. | (?:\w+\.(?!$))+ | \w+(?:[-\']\w*)* | [\.,?!]')
+    t_base = pattern.findall(x)
+    t = []
+    for token in t_base:
+        if (token.count('.') > 1 or token.count('.') < 1 or token == '.'):
+            t.append(token)
+        else:
+            t.append(token[:-1])
+            t.append(token[-1])
+    return t
+
+def question_processing2(question):
+    # Lemmar og tekur út stopporð ásamt ?, ! og kommu
+    doc = nlp(question)
+    jonathan = " ".join([token.lemma_ for token in doc])
+    joseph = custom_tokenizer(jonathan)
+    tokens_normalized = []
+    for token in joseph:
+        if token != ',' and token != '?' and token != '!' and token not in stop_words:
+            w, m = b.lookup(token)
+            tokens_normalized.append(m[0].ord)
+    res = []
+    # None birtist for some reason, tekið út
+    for val in tokens_normalized:
+        if val != None :
+            res.append(val)
+    return tokens_normalized
